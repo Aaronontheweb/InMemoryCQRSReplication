80b9426a162a1e18bea764e1636e5ff5f264bf58
Aaron Stannard <aaron@petabridge.com>
2019-04-08 11:56:58 -0500

    created some initial order book events and commands

---
+++ src/Akka.CQRS/Commands/GetOrderBookSnapshot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS.Commands
+{
+    /// <summary>
+    /// Query the current order book snapshot
+    /// </summary>
+    public class GetOrderBookSnapshot : IWithStockId
+    {
+        public GetOrderBookSnapshot(string stockId)
+        {
+            StockId = stockId;
+        }
+
+        public string StockId { get; }
+    }
+}
