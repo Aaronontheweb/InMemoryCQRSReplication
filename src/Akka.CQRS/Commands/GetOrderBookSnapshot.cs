78fb04c9fc61ee520228aeff58e2d1a6a1e1e279
Aaron Stannard <aaron@petabridge.com>
2019-04-08 18:11:16 -0500

    added more entity message types

--- src/Akka.CQRS/Commands/GetOrderBookSnapshot.cs
+++ src/Akka.CQRS/Commands/GetOrderBookSnapshot.cs
@@ -5,15 +5,34 @@ using System.Text;
 namespace Akka.CQRS.Commands
 {
     /// <summary>
+    /// Specifics the level of detail for an OrderBook snapshot.
+    /// </summary>
+    public enum DetailLevel
+    {
+        /// <summary>
+        /// Lists all of the details
+        /// </summary>
+        Full,
+
+        /// <summary>
+        /// Lists only the aggregates
+        /// </summary>
+        Summary
+    }
+
+    /// <summary>
     /// Query the current order book snapshot
     /// </summary>
     public class GetOrderBookSnapshot : IWithStockId
     {
-        public GetOrderBookSnapshot(string stockId)
+        public GetOrderBookSnapshot(string stockId, DetailLevel detail = DetailLevel.Summary)
         {
             StockId = stockId;
+            Detail = detail;
         }
 
         public string StockId { get; }
+
+        public DetailLevel Detail { get; }
     }
 }
---
+++ src/Akka.CQRS/PriceRange.cs
@@ -0,0 +1,21 @@
+﻿namespace Akka.CQRS
+{
+    /// <summary>
+    /// Represents a price band, typically weighted by buy/sell volume.
+    /// </summary>
+    public struct PriceRange
+    {
+        public PriceRange(decimal min, decimal mean, decimal max)
+        {
+            Min = min;
+            Mean = mean;
+            Max = max;
+        }
+
+        public decimal Min { get; }
+
+        public decimal Mean { get; }
+
+        public decimal Max { get; }
+    }
+}
