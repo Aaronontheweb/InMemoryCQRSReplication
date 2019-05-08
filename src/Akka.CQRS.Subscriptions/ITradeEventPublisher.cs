736ddced376b775ea5d2885323d4d8f9a61c96da
Aaron Stannard <aaron@petabridge.com>
2019-05-08 18:04:06 -0500

    working on subsriptions

---
+++ src/Akka.CQRS.Subscriptions/ITradeEventPublisher.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ITradeEventPublisher.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// Abstraction used for publishing data about <see cref="ITradeEvent"/> instances.
+    /// </summary>
+    public interface ITradeEventPublisher
+    {
+        void Publish(string tickerSymbol, ITradeEvent @event);
+    }
+
+    /// <summary>
+    /// Subscription to a specific ticker has been successful.
+    /// </summary>
+    public sealed class TradeSubscribeAck
+    {
+        public TradeSubscribeAck(string tickerSymbol, TradeEventType[] events)
+        {
+            TickerSymbol = tickerSymbol;
+            Events = events;
+        }
+
+        public string TickerSymbol { get; }
+
+        public TradeEventType[] Events { get; }
+    }
+
+    /// <summary>
+    /// Unsubscription to a specific ticker has been successful.
+    /// </summary>
+    public sealed class TradeUnsubscribeAck
+    {
+        public TradeUnsubscribeAck(string tickerSymbol, TradeEventType[] events)
+        {
+            TickerSymbol = tickerSymbol;
+            Events = events;
+        }
+
+        public string TickerSymbol { get; }
+
+        public TradeEventType[] Events { get; }
+    }
+
+    /// <summary>
+    /// Subscription to a specific ticker was not successful.
+    /// </summary>
+    public sealed class TradeSubscribeNack
+    {
+        public TradeSubscribeNack(string tickerSymbol, TradeEventType[] events, string reason)
+        {
+            TickerSymbol = tickerSymbol;
+            Events = events;
+            Reason = reason;
+        }
+
+        public string TickerSymbol { get; }
+
+        public TradeEventType[] Events { get; }
+
+        public string Reason { get; }
+    }
+}
---
+++ src/Akka.CQRS.TradeProcessor.Actors/OrderBookActor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.CQRS.Matching;
+using Akka.Persistence;
+
+namespace Akka.CQRS.TradeProcessor.Actors
+{
+    /// <summary>
+    /// Actor responsible for processing orders for a specific ticker symbol.
+    /// </summary>
+    public class OrderBookActor : ReceivePersistentActor
+    {
+        private MatchingEngine _matchingEngine;
+
+        public OrderBookActor(string tickerSymbol)
+        {
+            TickerSymbol = tickerSymbol;
+        }
+
+        public string TickerSymbol { get; }
+        public override string PersistenceId => TickerSymbol;
+    }
+}
