cbe6371b1f3e80445db6b73428967e21ee0666e6
Aaron Stannard <aaron@petabridge.com>
2019-05-09 11:22:16 -0500

    Akka.NET trading services (#4)
    * standardized versions of .NET Core used
    
    * created host process for actors for trade service
    
    * added reference to Akka.Persistence
    
    * working on subsriptions

---
+++ src/Akka.CQRS.Subscriptions/ITradeEventSubscriptionManager.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ITradeEventSubscriptionManager.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// Abstraction used to manage subscriptions for <see cref="ITradeEvent"/>s.
+    /// </summary>
+    public interface ITradeEventSubscriptionManager
+    {
+        Task<TradeSubscribeAck> Subscribe(string tickerSymbol, TradeEventType[] events, IActorRef subscriber);
+
+        Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, TradeEventType[] events, IActorRef subscriber);
+    }
+}
---
+++ src/Akka.CQRS.Subscriptions/TradeEventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// The type of trade event we're interested in.
+    /// </summary>
+    public enum TradeEventType
+    {
+        Bid,
+        Ask,
+        Fill,
+        Match
+    }
+}
