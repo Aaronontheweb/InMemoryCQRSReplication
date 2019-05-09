dc5056088ea83e36905554f3e772c48b93379c77
Aaron Stannard <aaron@petabridge.com>
2019-05-09 18:09:15 -0500

    fleshing out the OrderBookActor and its infrastructure

---
+++ src/Akka.CQRS.Subscriptions/NoOp/NoOpTradeEventSubscriptionManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Akka.CQRS.Subscriptions.NoOp
+{
+    /// <summary>
+    /// Used to ignore subscription management events.
+    /// </summary>
+    public sealed class NoOpTradeEventSubscriptionManager : ITradeEventSubscriptionManager
+    {
+        public static readonly NoOpTradeEventSubscriptionManager Instance = new NoOpTradeEventSubscriptionManager();
+        private NoOpTradeEventSubscriptionManager() { }
+
+        public Task<TradeSubscribeAck> Subscribe(string tickerSymbol, IActorRef subscriber)
+        {
+            return Subscribe(tickerSymbol, TradeEventHelpers.AllTradeEventTypes, subscriber);
+        }
+
+        public Task<TradeSubscribeAck> Subscribe(string tickerSymbol, TradeEventType @event, IActorRef subscriber)
+        {
+            return Subscribe(tickerSymbol, new []{ @event }, subscriber);
+        }
+
+        public Task<TradeSubscribeAck> Subscribe(string tickerSymbol, TradeEventType[] events, IActorRef subscriber)
+        {
+            return Task.FromResult(new TradeSubscribeAck(tickerSymbol, TradeEventHelpers.AllTradeEventTypes));
+        }
+
+        public Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, TradeEventType[] events, IActorRef subscriber)
+        {
+            return Task.FromResult(new TradeUnsubscribeAck(tickerSymbol, events));
+        }
+
+        public Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, TradeEventType @event, IActorRef subscriber)
+        {
+            return Unsubscribe(tickerSymbol, new[] {@event}, subscriber);
+        }
+
+        public Task<TradeUnsubscribeAck> Unsubscribe(string tickerSymbol, IActorRef subscriber)
+        {
+            return Unsubscribe(tickerSymbol, TradeEventHelpers.AllTradeEventTypes, subscriber);
+        }
+    }
+}
--- src/Akka.CQRS.TradeProcessor.Actors/OrderBookActor.cs
+++ src/Akka.CQRS.TradeProcessor.Actors/OrderBookActor.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
+using Akka.CQRS.Commands;
+using Akka.CQRS.Events;
 using Akka.CQRS.Matching;
 using Akka.CQRS.Subscriptions;
+using Akka.CQRS.Subscriptions.DistributedPubSub;
+using Akka.Event;
 using Akka.Persistence;
+using Akka.Persistence.Extras;
+using Akka.Util.Internal;
 
 namespace Akka.CQRS.TradeProcessor.Actors
 {
@@ -10,17 +19,33 @@ namespace Akka.CQRS.TradeProcessor.Actors
     /// </summary>
     public class OrderBookActor : ReceivePersistentActor
     {
+        /// <summary>
+        /// Take a snapshot every N messages persisted.
+        /// </summary>
+        public const int SnapshotInterval = 100;
         private MatchingEngine _matchingEngine;
         private readonly ITradeEventPublisher _publisher;
+        private readonly ITradeEventSubscriptionManager _subscriptionManager;
+        private readonly IActorRef _confirmationActor;
 
-        public OrderBookActor(string tickerSymbol, MatchingEngine matchingEngine, ITradeEventPublisher publisher)
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+
+        public OrderBookActor(string tickerSymbol, IActorRef confirmationActor) : this(tickerSymbol, null, DistributedPubSubTradeEventPublisher.For(Context.System), confirmationActor, subscriptionManager) { }
+        public OrderBookActor(string tickerSymbol, MatchingEngine matchingEngine, ITradeEventPublisher publisher, IActorRef confirmationActor, ITradeEventSubscriptionManager subscriptionManager)
         {
             TickerSymbol = tickerSymbol;
-            _matchingEngine = matchingEngine;
+            _matchingEngine = matchingEngine ?? CreateDefaultMatchingEngine(tickerSymbol, _log);
             _publisher = publisher;
+            _confirmationActor = confirmationActor;
+            _subscriptionManager = subscriptionManager;
 
             Recovers();
             Commands();
+        }
+
+        private static MatchingEngine CreateDefaultMatchingEngine(string tickerSymbol, ILoggingAdapter logger)
+        {
+            return new MatchingEngine(tickerSymbol, logger);
         }
 
         public string TickerSymbol { get; }
@@ -28,12 +53,83 @@ namespace Akka.CQRS.TradeProcessor.Actors
 
         private void Recovers()
         {
-            
+            Recover<SnapshotOffer>(offer =>
+            {
+                if (offer.Snapshot is OrderbookSnapshot orderBook)
+                {
+                    _matchingEngine = MatchingEngine.FromSnapshot(orderBook, _log);
+                }
+            });
+
+            Recover<Bid>(b => { _matchingEngine.WithBid(b); });
+            Recover<Ask>(a => { _matchingEngine.WithAsk(a);});
+
+            // Fill and Match can't modify the state of the MatchingEngine.
+            Recover<Match>(m => { }); 
+            Recover<Fill>(f => { });
         }
 
         private void Commands()
         {
-            
+            Command<ConfirmableMessage<Ask>>(a =>
+            {
+                
+                // For the sake of efficiency - update orderbook and then persist all events
+                var events = _matchingEngine.WithAsk(a.Message);
+                var persistableEvents = new ITradeEvent[] {a.Message}.Concat<ITradeEvent>(events); // ask needs to go before Fill / Match
+
+                PersistAll(persistableEvents, @event =>
+                {
+                    _log.Info("[{0}][{1}] - {2} units @ {3} per unit", PersistenceId, @event.ToTradeEventType(), a.Message.AskQuantity, a.Message.AskPrice);
+                    if (@event is Ask)
+                    {
+                        _confirmationActor.Tell(new Confirmation(a.ConfirmationId, PersistenceId));
+                    }
+                    _publisher.Publish(PersistenceId, @event);
+
+                    // Take a snapshot every N messages to optimize recovery time
+                    if (LastSequenceNr % SnapshotInterval == 0)
+                    {
+                        SaveSnapshot(_matchingEngine.GetSnapshot());
+                    }
+                });
+            });
+
+            Command<ConfirmableMessage<Bid>>(b =>
+            {
+                // For the sake of efficiency -update orderbook and then persist all events
+                var events = _matchingEngine.WithBid(b.Message);
+                var persistableEvents = new ITradeEvent[] { b.Message }.Concat<ITradeEvent>(events); // bid needs to go before Fill / Match
+
+                PersistAll(persistableEvents, @event =>
+                {
+                    _log.Info("[{0}][{1}] - {2} units @ {3} per unit", PersistenceId, @event.ToTradeEventType(), b.Message.BidQuantity, b.Message.BidPrice);
+                    if (@event is Bid)
+                    {
+                        _confirmationActor.Tell(new Confirmation(b.ConfirmationId, PersistenceId));
+                    }
+                    _publisher.Publish(PersistenceId, @event);
+
+                    // Take a snapshot every N messages to optimize recovery time
+                    if (LastSequenceNr % SnapshotInterval == 0)
+                    {
+                        SaveSnapshot(_matchingEngine.GetSnapshot());
+                    }
+                });
+            });
+
+            /*
+             * Handle subscriptions directly in case we're using in-memory, local pub-sub.
+             */
+            Command<TradeSubscribe>(sub =>
+            {
+                
+            });
+
+            Command<GetOrderBookSnapshot>(s =>
+            {
+                Sender.Tell(_matchingEngine.GetSnapshot());
+            });
         }
     }
 }
