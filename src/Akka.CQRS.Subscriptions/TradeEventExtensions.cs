40e66c15cc21f5aeede9ae656fe2a709f3ae6724
Aaron Stannard <aaron@petabridge.com>
2019-05-09 14:14:26 -0500

    forgot to add on previous commit

---
+++ src/Akka.CQRS.Subscriptions/TradeEventExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Akka.CQRS.Events;
+
+namespace Akka.CQRS.Subscriptions
+{
+    /// <summary>
+    /// Extension methods for working with <see cref="ITradeEvent"/>
+    /// </summary>
+    public static class TradeEventExtensions
+    {
+        public static TradeEventType ToTradeEventType(this ITradeEvent @event)
+        {
+            switch (@event)
+            {
+                case Bid b:
+                    return TradeEventType.Bid;
+                case Ask a:
+                    return TradeEventType.Ask;
+                case Fill f:
+                    return TradeEventType.Fill;
+                case Match m:
+                    return TradeEventType.Match;
+                default:
+                    throw new ArgumentOutOfRangeException($"[{@event}] is not a supported trade event type.", nameof(@event));
+            }
+        }
+    }
+}
