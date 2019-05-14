078d2bee0e3536671e7fa3211f291ec96c9fbcee
Aaron Stannard <aaron@petabridge.com>
2019-05-14 10:44:03 -0500

    working on pricing views and events

---
+++ src/Akka.CQRS.Pricing/Events/IPriceUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS.Pricing.Events
+{
+    /// <summary>
+    /// Used to signal a change in price for a specific stock.
+    /// </summary>
+    public interface IPriceUpdate : IWithStockId
+    {
+        /// <summary>
+        /// The time of this price update.
+        /// </summary>
+        DateTimeOffset Timestamp { get; }
+
+        /// <summary>
+        /// The current volume-weighted average price.
+        /// </summary>
+        decimal CurrentAvgPrice { get; }
+    }
+
+    /// <summary>
+    /// Concrete 
+    /// </summary>
+    public sealed class PriceChanged : IPriceUpdate
+    {
+        public DateTimeOffset Timestamp { get; }
+
+        public decimal CurrentAvgPrice { get; }
+
+        public string StockId { get; }
+    }
+}
---
+++ src/Akka.CQRS.Pricing/Views/PriceHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS.Pricing
+{
+    /// <summary>
+    /// Details the price history for a specific ticker symbol.
+    /// </summary>
+    public sealed class PriceHistory
+    {
+        
+    }
+}
