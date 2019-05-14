0bfb57bf7050b947d98ca726cca0a5671b469c9a
Aaron Stannard <aaron@petabridge.com>
2019-05-14 17:20:38 -0500

    working on in-memory replication now

---
+++ src/Akka.CQRS.Pricing.Actors/PriceViewActor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace Akka.CQRS.Pricing.Actors
+{
+    /// <summary>
+    /// In-memory, replicated view of the current price and volume for a specific stock.
+    /// </summary>
+    public sealed class PriceVolumeViewActor : ReceiveActor, IWithStockId
+    {
+        private readonly string _tickerSymbol;
+
+        // the Cluster.Sharding proxy
+        private readonly IActorRef _priceActorGateway;
+
+        // the DistributedPubSub mediator
+        private readonly IActorRef _mediator;
+
+        private IActorRef _tickerEntity;
+
+        public PriceVolumeViewActor(string tickerSymbol, IActorRef priceActorGateway, IActorRef mediator)
+        {
+            _tickerSymbol = tickerSymbol;
+            _priceActorGateway = priceActorGateway;
+            _mediator = mediator;
+        }
+    }
+}
---
+++ src/Akka.CQRS.Pricing/Commands/FetchPriceAndVolume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS.Pricing.Commands
+{
+    /// <summary>
+    /// Fetch the N most recent price and volume updates for a specific ticker symbol.
+    /// </summary>
+    public sealed class FetchPriceAndVolume : IWithStockId
+    {
+        public FetchPriceAndVolume(string stockId)
+        {
+            StockId = stockId;
+        }
+
+        public string StockId { get; }
+    }
+
+    public sealed class GetCurrentPrice : IWithStockId
+    {
+        public string StockId { get; }
+    }
+}
