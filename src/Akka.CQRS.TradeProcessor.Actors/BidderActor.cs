ab3f39461392560bb06b67fc3fae2eb6df3c94e9
Aaron Stannard <aaron@petabridge.com>
2019-05-13 21:15:22 -0500

    adding bidder and asker actors

---
+++ src/Akka.CQRS.TradeProcessor.Actors/BidderActor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.CQRS.Events;
+using Akka.CQRS.Subscriptions;
+using Akka.Event;
+using Akka.Util;
+
+namespace Akka.CQRS.TradeProcessor.Actors
+{
+    /// <summary>
+    /// Actor that randomly places bids for a specific ticker symbol.
+    /// </summary>
+    public sealed class BidderActor : ReceiveActor
+    {
+        private readonly string _tickerSymbol;
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+        private readonly ITradeEventSubscriptionManager _subscriptionManager;
+
+        // tradeGateway is usefully going to be a Cluster.Sharding.RegionProxy
+        private readonly IActorRef _tradeGateway;
+        private readonly PriceRange _targetRange;
+        private readonly Dictionary<string, Bid> _bids = new Dictionary<string, Bid>();
+        private readonly List<Fill> _fills = new List<Fill>();
+        private ICancelable _bidInterval;
+
+        private class DoSubscribe
+        {
+            public static readonly DoSubscribe Instance = new DoSubscribe();
+            private DoSubscribe() { }
+        }
+
+        private class DoBid
+        {
+            public static readonly DoBid Instance = new DoBid();
+            private DoBid() { }
+        }
+
+        public BidderActor(string tickerSymbol, ITradeEventSubscriptionManager subscriptionManager, IActorRef tradeGateway, PriceRange targetRange)
+        {
+            _tickerSymbol = tickerSymbol;
+            _subscriptionManager = subscriptionManager;
+            _tradeGateway = tradeGateway;
+            _targetRange = targetRange;
+            Self.Tell(DoSubscribe.Instance);
+            Subscribing();
+        }
+
+        private void Subscribing()
+        {
+            ReceiveAsync<DoSubscribe>(async _ =>
+                {
+                    try
+                    {
+                        var ack = await _subscriptionManager.Subscribe(_tickerSymbol, TradeEventType.Fill, Self);
+                        Become(Bidding);
+                        _bidInterval = Context.System.Scheduler.ScheduleTellRepeatedlyCancelable(TimeSpan.FromSeconds(1),
+                            TimeSpan.FromSeconds(10), Self, DoBid.Instance, ActorRefs.NoSender);
+                    }
+                    catch (Exception ex)
+                    {
+                        _log.Error(ex, "Error while waiting for SubscribeAck for [{0}-{1}] - retrying in 5s.", _tickerSymbol, TradeEventType.Fill);
+                        Context.System.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(5), Self, DoSubscribe.Instance, ActorRefs.NoSender);
+                    }
+                });
+        }
+
+        private void Bidding()
+        {
+            // Time to place a new bid
+            Receive<DoBid>(_ =>
+            {
+                var price = ThreadLocalRandom.Current.WithinRange(_targetRange);
+                var quantity = ThreadLocalRandom.Current.Next(1, 20);
+                
+            });
+        }
+
+        protected override void PostStop()
+        {
+            _bidInterval?.Cancel();
+        }
+    }
+}
---
+++ src/Akka.CQRS/PriceRangeExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.CQRS
+{
+    /// <summary>
+    /// Utility class for helping generate <see cref="Random"/> <see cref="decimal"/> values
+    /// within a <see cref="PriceRange"/>.
+    /// </summary>
+    public static class PriceRangeExtensions
+    {
+        public static decimal WithinRange(this Random r, PriceRange range)
+        {
+            var sample = NextDecimalSample(r);
+            return range.Max * sample + range.Min * (1 - sample);
+        }
+
+        /*
+         * Random RNG algorithms provided by Jon Skeet's answer: https://stackoverflow.com/a/609529/377476
+         * And by Bryan Loeper's answer: https://stackoverflow.com/a/28860710/377476
+         *
+         */
+        public static int NextInt32(this Random rng)
+        {
+            var firstBits = rng.Next(0, 1 << 4) << 28;
+            var lastBits = rng.Next(0, 1 << 28);
+            return firstBits | lastBits;
+        }
+
+        public static decimal NextDecimalSample(this Random random)
+        {
+            var sample = 1m;
+            //After ~200 million tries this never took more than one attempt but it is possible to generate combinations of a, b, and c with the approach below resulting in a sample >= 1.
+            while (sample >= 1)
+            {
+                var a = random.NextInt32();
+                var b = random.NextInt32();
+                //The high bits of 0.9999999999999999999999999999m are 542101086.
+                var c = random.Next(542101087);
+                sample = new decimal(a, b, c, false, 28);
+            }
+            return sample;
+        }
+    }
+}
