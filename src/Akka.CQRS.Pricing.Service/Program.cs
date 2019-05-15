4419eb38209584b418d69c99dc8612827157a2db
Aaron Stannard <aaron@petabridge.com>
2019-05-14 21:50:42 -0500

    adding pricing cluster

---
+++ src/Akka.CQRS.Pricing.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akka.CQRS.Pricing.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
