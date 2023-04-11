1c1923652e0e27d9ec3a17ed4716698228847a7e
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-04-12 03:48:13 +0700

    Replace Akka.Persistence.MongoDb with Akka.Persistence.Sql

---
+++ src/Akka.CQRS.Infrastructure/SqlDbHoconHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Akka.Configuration;
+
+namespace Akka.CQRS.Infrastructure
+{
+    /// <summary>
+    /// Shared utility class for formatting SQL connection strings into the required
+    /// Akka.Persistence.Sql HOCON <see cref="Akka.Configuration.Config"/>.
+    /// </summary>
+    public static class SqlDbHoconHelper
+    {
+        public static Configuration.Config GetSqlHocon(string connectionStr, string providerName)
+        {
+            var hocon = $@"
+akka.persistence.journal.sql {{
+    connection-string = ""{connectionStr}""
+    provider-name = ""{providerName}""
+}}
+akka.persistence.query.journal.sql {{
+    connection-string = ""{connectionStr}""
+    provider-name = ""{providerName}""
+}}
+akka.persistence.snapshot-store.sql{{
+    connection-string = ""{connectionStr}""
+    provider-name = ""{providerName}""
+}}";
+            return hocon;
+        }
+    }
+}
