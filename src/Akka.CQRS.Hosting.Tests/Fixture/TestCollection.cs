1386ef5bfa787b6c1c25766d8911a48074e1c35b
Ebere Abanonu <eaba@users.noreply.github.com>
2023-04-18 18:39:28 +0100

    more changes to `Akka.CQRS.Hosting.Tests`

---
+++ src/Akka.CQRS.Hosting.Tests/Fixture/TestCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace Akka.CQRS.Hosting.Tests.Fixture
+{
+    [CollectionDefinition(nameof(TestCollection), DisableParallelization = true)]
+    public class TestCollection : ICollectionFixture<TestFixture>
+    {
+    }
+}
