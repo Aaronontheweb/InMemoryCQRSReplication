d14bc578a8e399038a71be9dd9969271611b5361
Ebere Abanonu <eaba@users.noreply.github.com>
2023-04-22 16:55:25 +0100

    updates

--- build.fsx
+++ build.fsx
@@ -84,7 +84,30 @@ module internal ResultHandling =
         buildErrorMessage
         >> Option.iter (failBuildWithMessage errorLevel)
 
-Target "RunTests" (fun _ ->
+Target "RunTestsWindows" (fun _ ->
+    let projects = 
+        match (isWindows) with 
+        | true -> !! "./src/**/*.Tests.csproj"
+        | _ -> !! "./src/**/*.Tests.csproj" // if you need to filter specs for Linux vs. Windows, do it here
+
+
+    let runSingleProject project =
+        let arguments =
+            match (hasTeamCity) with
+            | true -> (sprintf "test -c Release --no-build --logger:trx --logger:\"console;verbosity=normal\" --results-directory %s -- -parallel none -teamcity" (outputTests))
+            | false -> (sprintf "test -c Release --no-build --logger:trx --logger:\"console;verbosity=normal\" --results-directory %s -- -parallel none" (outputTests))
+
+        let result = ExecProcess(fun info ->
+            info.FileName <- "dotnet"
+            info.WorkingDirectory <- (Directory.GetParent project).FullName
+            info.Arguments <- arguments) (TimeSpan.FromMinutes 30.0) 
+        
+        ResultHandling.failBuildIfXUnitReportedError TestRunnerErrorLevel.Error result  
+
+    projects |> Seq.iter (log)
+    projects |> Seq.iter (runSingleProject)
+)
+Target "RunTestsUbuntu" (fun _ ->
     let projects = 
         match (isWindows) with 
         | true -> !! "./src/**/*.Tests.csproj"
@@ -108,7 +131,6 @@ Target "RunTests" (fun _ ->
     projects |> Seq.iter (log)
     projects |> Seq.iter (runSingleProject)
 )
-
 Target "NBench" <| fun _ ->
     let projects = 
         match (isWindows) with 
@@ -349,7 +371,8 @@ Target "Help" <| fun _ ->
       " * Build         Builds"
       " * Nuget         Create and optionally publish nugets packages"
       " * SignPackages  Signs all NuGet packages, provided that the following arguments are passed into the script: SignClientSecret={secret} and SignClientUser={username}"
-      " * RunTests      Runs tests"
+      " * RunTestsWindows      Runs tests"
+      " * RunTestsUbuntu      Runs tests"
       " * All           Builds, run tests, creates and optionally publish nuget packages"
       " * DocFx         Creates a DocFx-based website for this solution"
       ""
@@ -370,7 +393,8 @@ Target "Nuget" DoNothing
 "Clean" ==> "AssemblyInfo" ==> "Build" ==> "BuildRelease"
 
 // tests dependencies
-"Build" ==> "RunTests"
+"Build" ==> "RunTestsWindows"
+"Build" ==> "RunTestsUbuntu"
 
 // nuget dependencies
 "Clean" ==> "Build" ==> "CreateNuget"
@@ -384,7 +408,8 @@ Target "Nuget" DoNothing
 
 // all
 "BuildRelease" ==> "All"
-"RunTests" ==> "All"
+"RunTestsWindows" ==> "All"
+"RunTestsUbuntu" ==> "All"
 "NBench" ==> "All"
 "Nuget" ==> "All"
 
